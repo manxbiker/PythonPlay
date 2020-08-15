ef1e780939856e8b93cf6ae7b155b0f9e5578030
Andrew Reid <lists@arspamcontrol.com>
2020-08-15 15:36:56 +1200

    Experimenting with numpy for oneliners

---
+++ Python One Liners/test1.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([  [0, 1, 2, 3],
+                [4, 5, 6, 7],
+                [8, 9, 10, 11],
+                [12, 13, 14, 15]])
+
+print(a *1.6)
+print(a.dtype)
+b = a*1.6
+print(b.dtype)
